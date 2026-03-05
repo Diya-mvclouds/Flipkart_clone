73a742c86d68caa9a63b2aa40742af44655e228b
Diya-mvclouds <diyashahgu@gmail.com>
2026-03-05 16:56:06 +0530

    Delete

--- Day 3 Task 9 capital in front.js
+++
@@ -1,8 +0,0 @@
-function capToFront(str) {
-  let upper = "", lower = "";
-  for (let ch of str) {
-    ch === ch.toUpperCase() ? upper += ch : lower += ch;
-  }
-  return upper + lower;
-}
-console.log(capToFront('GoodDay'))
