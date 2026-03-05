eb6842d229374013aba2208a0d14d256cef281d1
Diya-mvclouds <diyashahgu@gmail.com>
2026-03-05 16:57:01 +0530

    Delete

--- Day 3 Taks 13 ASCII transform.js
+++
@@ -1,9 +0,0 @@
-function ascii(str){
-    let a = "";
-    for (const char of str){
-        let ch = char.charCodeAt(0);
-        a += ch % 2 == 0 ? char.toUpperCase() : char.toLowerCase();
-    }
-    return a;
-}
-console.log(ascii('LITTLE'))
