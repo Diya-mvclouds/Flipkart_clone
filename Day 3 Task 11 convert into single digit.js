a000c02016f6675c6842f851c3e98851ced53c01
Diya-mvclouds <diyashahgu@gmail.com>
2026-03-05 16:57:46 +0530

    Delete

--- Day 3 Task 11 convert into single digit.js
+++
@@ -1,9 +0,0 @@
-function finalSingleDigit(...numbers){
-    let sum = numbers.reduce((total,num) => total + num, 0);
-    console.log("Sum:", sum);
-    while (sum >= 10){
-        sum = sum.toString().split('').reduce((product, digit) => product * Number(digit), 1);
-        console.log("Product", sum);
-    }
-}
-console.log(finalSingleDigit(16,28));
